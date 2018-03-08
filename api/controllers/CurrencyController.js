b0b539d16b18e1d4d826e2826188b1df2dd2a886
Andrey Mishchenko <msav@msav.ru>
2018-03-08 23:24:52 +0300

    Added currency rates list response

--- api/controllers/CurrencyController.js
+++ api/controllers/CurrencyController.js
@@ -1,3 +1,5 @@
+const dateFormat = require('dateformat');
+
 /**
  * CurrencyController
  *
@@ -6,6 +8,36 @@
  */
 
 module.exports = {
-	
+  index: (req, res, next) => {
+    Currency.find().then(
+      (currencyList) => {
+
+        // Prepare the result records
+        //
+        let rows = [];
+        let currencyInfo = null;
+        for (let val of currencyList) {
+          let rateDate = new Date(val.rateDate);
+          currencyInfo = {
+            charCode: val.charCode,
+            nameRu: val.nameRu,
+            nameEn:val.nameEn,
+            cbrId: val.vendorId,
+            numCode: val.numCode,
+            nominal: val.nominal,
+            rate: val.rate,
+            rateDate: dateFormat(rateDate, 'dd.mm.yyyy')
+          };
+
+          rows.push(currencyInfo);
+        }
+
+        return res.json(rows);
+      },
+      (err) => {
+        return next(err);
+      }
+    );
+  }
 };
 
